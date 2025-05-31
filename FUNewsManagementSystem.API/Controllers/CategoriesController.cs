30fe27aa1a7c68ecb209f41ae0070076633841a6
Nguyen Quang Hau <quanghau6622@gmail.com>
2025-05-31 20:38:01 +0700

    Add layers for Category and Define API

---
+++ FUNewsManagementSystem.API/Controllers/CategoriesController.cs
@@ -0,0 +1,52 @@
+﻿using FUNewsManagementSystem.Application.Services;
+using FUNewsManagementSystem.Core.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FUNewsManagementSystem.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _service;
+
+        public CategoriesController(ICategoryService service) => _service = service;
+
+        [HttpGet]
+        public IActionResult GetAll() => Ok(_service.GetAll());
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var category = _service.GetById(id);
+            if (category == null) return NotFound();
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            _service.Add(category);
+            return CreatedAtAction(nameof(GetById), new { id = category.Id }, category);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Category category)
+        {
+            if (id != category.Id) return BadRequest();
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            _service.Update(category);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var result = _service.Delete(id);
+            return result ? NoContent() : BadRequest("Cannot delete: Category is used or not found.");
+        }
+    }
+
+}
---
+++ FUNewsManagementSystem.Application/Services/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using FUNewsManagementSystem.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNewsManagementSystem.Application.Services
+{
+    public interface ICategoryService
+    {
+        IEnumerable<Category> GetAll();
+        Category GetById(int id);
+        bool Add(Category category);
+        bool Update(Category category);
+        bool Delete(int id);
+    }
+
+}
